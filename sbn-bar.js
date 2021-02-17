767003ba8b53e56f1d7951151b86f98f11ee6a9a
mdrum <mdrum@psi.edu>
2021-02-17 15:55:01 -0700

    update for production

--- sbn-bar.js
+++ sbn-bar.js
@@ -1,27 +1,30 @@
-var makeRequest = function(url, sourceElement) {
-    xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4) {
-            if (this.status == 200) {
-                sourceElement.innerHTML = this.responseText;
+(function(document, window) {
+    
+    var makeRequest = function(url, sourceElement) {
+        xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4) {
+                if (this.status == 200) {
+                    sourceElement.innerHTML = this.responseText;
 
+                    var parent = sourceElement.parentNode
+                    while (sourceElement.firstChild) { parent.insertBefore(sourceElement.firstChild, sourceElement)}
+                    parent.removeChild(sourceElement)    
+                }
             }
-            if (this.status == 404) {
-                sourceElement.innerHTML = "Included html not found.";
-            }
-
-            // unwrap
-            var parent = sourceElement.parentNode
-            while (sourceElement.firstChild) { parent.insertBefore(sourceElement.firstChild, sourceElement)}
-            parent.removeChild(sourceElement)
-
-            window.dispatchEvent(new Event("PDSSBN_contentLoaded"))
         }
+        xhttp.open("GET", url, true);
+        xhttp.send();
     }
-    xhttp.open("GET", url, true);
-    xhttp.send();
-}
 
-let sbnheader = document.createElement('header')
-document.body.prepend(sbnheader)
-makeRequest('sbn/sbn-bar.html', sbnheader)
+    let sbnheader = document.createElement('header')
+    document.body.prepend(sbnheader)
+    makeRequest('https://sbn.psi.edu/sbn-bar/sbn-bar.html', sbnheader)
+
+    let css = document.createElement('link');
+    css.href = 'https://sbn.psi.edu/sbn-bar/sbn-bar.css';
+    css.rel = 'stylesheet';
+    css.type = 'text/css';
+    document.body.appendChild(css)
+
+})();
